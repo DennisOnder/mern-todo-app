06b2d5a9a0fd9b29885f527df072898fc714c96b
Dennis Onder <webdev.dennisonder@gmail.com>
2018-10-27 22:04:52 +0200

    Prepared the dashboard

---
+++ client/src/components/Auth.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div id="Auth">
+        <h1>Just Another Todo App</h1>
+        <div>
+          <button>Log In</button>
+          <button>Register</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Auth;
